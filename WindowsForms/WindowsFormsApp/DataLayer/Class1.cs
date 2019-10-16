5ff99206fcbea5a5986ff0fbe2a86a1ac06a0079
unknown <rok.grgec@gmail.com>
2019-10-16 21:32:14 +0200

    start on winforms project

---
+++ WindowsForms/WindowsFormsApp/DataLayer/Class1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using RestSharp;
+
+namespace DataLayer
+{
+    public static class Contributor
+    {
+
+        private const string TeamsUrl = "https://world-cup-json-2018.herokuapp.com/teams";
+        private const string MatchesUrl = "https://world-cup-json-2018.herokuapp.com/matches/country?fifa_code=";
+        private const string ResultsUrl = "https://world-cup-json-2018.herokuapp.com/teams/results";
+
+        public static async Task<string> GetUrl (string url)
+        {
+            var client = new RestClient(url);
+            var response = await client.ExecuteTaskAsync(new RestRequest());
+
+            return response.Content;
+        }
+
+        public static async Task<List<string>> TeamNames()
+        {
+            dynamic response = JsonConvert.DeserializeObject(await Task.Run(() => GetUrl(TeamsUrl)));
+            List<string> country = new List<string>();
+            foreach (var s in response)
+            {
+                country.Add((string)s.country);
+            }
+            return country;
+        }
+
+        public static async Task<string> CountryFifaCode(string chosenTeam)
+        {
+            await GetUrl(TeamsUrl);
+            dynamic response = JsonConvert.DeserializeObject(await Task.Run(() => GetUrl(TeamsUrl)));
+            foreach (var s in response)
+            {
+                if (s.fifa_code == chosenTeam)
+                {
+                    return s.fifa_code;
+                }
+            }
+            throw new System.ArgumentException($"Country: {chosenTeam} doesn't exist");
+        }
+
+        public static async Task<List<string>> GetMatchFirstEleven(string fifa_id, string fifa_code)
+        {
+            dynamic response = JsonConvert.DeserializeObject(await Task.Run(() => GetUrl(MatchesUrl + fifa_code)));
+            List<string> starting_eleven = new List<string>();
+            foreach (var match in response)
+            {
+                if (match.fifa_id == fifa_id)
+                {
+
+                    if (match.home_team.code == fifa_code)
+                    {
+                        foreach (var player in match.home_team_statistics.starting_eleven)
+                        {
+                            starting_eleven.Add((string)player.name);
+                        }
+                        return starting_eleven;
+                    }
+                    else
+                    {
+                        foreach (var player in match.away_team_statistics.starting_eleven)
+                        {
+
+                            starting_eleven.Add((string)player.name);
+                        }
+                        return starting_eleven;
+                    }
+                }
+            }
+            throw new ArgumentException("Problems fetching first eleven, maybe your fifa_id is wrong?");
+        }
+
+        public static async Task<dynamic> GetCountryMatches(string fifa_code)
+        {
+            return JsonConvert.DeserializeObject(await Task.Run(() => GetUrl(MatchesUrl + fifa_code)));
+        }
+
+        public static async Task<dynamic> CountryMatchResults(string countryName)
+        {
+            dynamic response = JsonConvert.DeserializeObject(await Task.Run(() => GetUrl(ResultsUrl)));
+            foreach (var s in response)
+            {
+                if (s.country == countryName)
+                {
+                    return s;
+                }
+            }
+            throw new ArgumentException($"Cant find {countryName} results");
+        }
+
+
+    }
+
+}
---
+++ WindowsForms/WindowsFormsApp/WindowsFormsApp/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            TeamLoader();
+        }
+
+        public async void ShowTeamBox()
+        {
+            comboBox1.Show();
+            label1.Show();
+        }
+
+
+
+        public async void TeamLoader()
+        {
+            List<string> countries = await Task.Run(() => DataLayer.Contributor.TeamNames());
+            ShowTeamBox();
+            comboBox1.DataSource = countries;
+            comboBox1.SelectedItem = countries[0];
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
