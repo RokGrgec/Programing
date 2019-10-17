da11924b3e02c3df3e2ac4cd0ddf9dc21b5a537d
unknown <rok.grgec@gmail.com>
2019-10-17 23:07:00 +0200

    worked on project

---
+++ WindowsForms/WindowsFormsApp/DataLayer/PlayerInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class PlayerInfo
+    {
+        public string player_name { get; set; }
+        public int player_number { get; set; }
+        public string position { get; set; }
+        public bool is_captain { get; set; }
+        public bool is_fav_player { get; set; }
+
+        public override string ToString() => $"{player_name} | {player_number} | {position} | {(is_captain ? "true" : "false")}";
+        
+    }
+}
---
+++ WindowsForms/WindowsFormsApp/WindowsFormsApp/ChoseFavPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataLayer;
+using System.IO;
+
+namespace WindowsFormsApp
+{
+    public partial class ChoseFavPlayer : Form
+    {
+        private Form1 form;
+        public ChoseFavPlayer()
+        {
+            InitializeComponent();
+            this.form = form;
+        }
+
+        private async void FillPlayers(Dictionary<string,PlayerInfo> players)
+        {
+            foreach (var player in players)
+            {
+                checkedListBox1.Items.Add(player);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
